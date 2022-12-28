108b9cd310534ed7359ffa8939d8044a304d52fa
luckiikawayii <luckiikawayii@outlook.com>
2022-12-28 20:20:58 +0800

    update: web

--- examples/cpp/test-class8.c
+++ examples/cpp/test-class8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    int a[2][3] = {{1, 2, 3}, {4, 5, 6}}, b[3][2], i, j;
+    int a[2][3] = {{1, 2, 3}, {3, 2, 1}}, b[3][2], i, j;
     printf("请输出a的元素：\n");
     for (i = 0; i <= 1; i++)
     {
@@ -12,6 +12,8 @@ int main(void)
         }
         printf("\n");
     }
+
+    return 0;
     printf("请输出b的元素:\n");
     for (j = 0; j <= 2; j++)
     {
@@ -21,5 +23,4 @@ int main(void)
         }
         printf("\n");
     }
-    return 0;
 }
